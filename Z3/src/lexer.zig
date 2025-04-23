c0eb7a4d565a299ff868c9a6dba7be3ee821e2ac
Vinny <vincentni2007@gmail.com>
2025-04-23 13:39:09 -0400

    mid lexer implementation :thumbsup:

--- Z3/src/lexer.zig
+++ Z3/src/lexer.zig
@@ -4,7 +4,7 @@ const stdout = std.io.getStdOut().writer();
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
-const TokenType = enum {
+const Token = union(enum) {
     // general syntax
     LParen,
     RParen,
@@ -16,22 +16,22 @@ const TokenType = enum {
     Comma,
     Hashtag,
     Shebang,
+    // Bang,
     Dollar,
     Semicolon,
     DoubleQuote,
     SingleQuote,
-    EscapedChar,
 
     // keywords (will probably end up saving these for a parser)
-    If,
-    Else,
-    Switch,
-    Case,
-    While,
-    Do,
-    For,
-    Break,
-    Return,
+    // If,
+    // Else,
+    // Switch,
+    // Case,
+    // While,
+    // Do,
+    // For,
+    // Break,
+    // Return,
 
     // arithmetic
     Plus,
@@ -55,7 +55,7 @@ const TokenType = enum {
     // OR, // |
     XOR, // ^
     NOT, // \!
-    // LSHIFT, // <<
+    LSHIFT, // <<
     // RSHIFT, // >>
 
     // bitwise assignment
@@ -67,7 +67,7 @@ const TokenType = enum {
 
     // conditional logic
     IF_Equal, // ==
-    IF_NotEqual, // !=
+    // IF_NotEqual, // !=
     // LessThan,
     // GreaterThan,
     LE, // Less than or equal to
@@ -81,200 +81,112 @@ const TokenType = enum {
     DoubleRightCarrot, // bitshift right; concat to a file 
     Ampersand, // bitwise AND; shell background process (a & b); (command &)
     Pipe, // bitwise or; Piping (a | b), (command | command)
+    // Bang, // bang; not (!var), (!command)
+    BangNotEqual, // can be ! (not), != (if not equal), or not then assign
 
-    Literal
+    Literal: []const u8,
+    EscapedChar: u8,
 };
 
-const Token = struct {
-    TokenType: TokenType,
-    Value: []const u8
-};
+const Tokenizer = struct {
+    i: u8,
+    input: []const u8,
+    out: std.ArrayList(Token),
 
-fn createToken(Type: TokenType, Value: []const u8) Token {
-    return Token {
-        .TokenType = Type,
-        .Value = Value
-    };
-}
-
-fn peek(input: []const u8, i: u32) ?u8 {
-    if (i + 1 >= input.len) {
-        return null;
+    pub fn init(self: *Tokenizer) void {
+        errdefer self.out.deinit();
     }
 
-    return input[i + 1];
-}
-
-fn printTokens(input: std.ArrayList(Token)) void {
-    for (input.items) |token| {
-        std.debug.print("{s}: {s}\n", .{@tagName(token.TokenType), token.Value});
-    }
-}
-
-pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
-    var outputTokens = std.ArrayList(Token).init(allocator);
-    errdefer outputTokens.deinit();
-
-    var i: u32 = 0;
-    while (i < input.len) {
-        const c = input[i];
-        switch (c) {
-            '(' => {
-                try outputTokens.append(createToken(TokenType.LParen, ""));
-            },
-            ')' => {
-                try outputTokens.append(createToken(TokenType.RParen, ""));
-            },
-            '[' => {
-                try outputTokens.append(createToken(TokenType.LBracket, ""));
-            },
-            ']' => {
-                try outputTokens.append(createToken(TokenType.RBracket, ""));
-            },
-            '{' => {
-                try outputTokens.append(createToken(TokenType.LCurly, ""));
-            },
-            '}' => {
-                try outputTokens.append(createToken(TokenType.RCurly, ""));
-            },
-            '.' => {
-                try outputTokens.append(createToken(TokenType.Dot, ""));
-            },
-            ',' => {
-                try outputTokens.append(createToken(TokenType.Comma, ""));
-            },
-            '#' => {
-                if (i + 1 >= input.len) {
-                    try outputTokens.append(createToken(TokenType.Hashtag, ""));
-                    break;
-                }
-
-                if (input[i + 1] == '!') {
-                    try outputTokens.append(createToken(TokenType.Shebang, ""));
-                    i += 1;
-                }
-                else {
-                    try outputTokens.append(createToken(TokenType.Hashtag, ""));
-                }
-            },
-            '$' => {
-                try outputTokens.append(createToken(TokenType.Dollar, ""));
-            },
-            ';' => {
-                try outputTokens.append(createToken(TokenType.Semicolon, ""));
-            },
-            '\"' => {
-                try outputTokens.append(createToken(TokenType.DoubleQuote, ""));
-            },
-            '\'' => {
-                try outputTokens.append(createToken(TokenType.SingleQuote, ""));
-            },
-            '|' => {
-                try outputTokens.append(createToken(TokenType.Pipe, ""));
-            },
-            '!' => {
-                try outputTokens.append(createToken(TokenType.NOT, ""));
-            },
-            // ' ' => {
-            //     try outputTokens.append(createToken(TokenType.Space, ""));
-            // },
-            '\\' => {
-                if (peek(input, i)) |s| {                    
-                    try outputTokens.append(createToken(TokenType.EscapedChar, &[1]u8 {s}));
-                    i += 1;
-                } else {
-                    try outputTokens.append(createToken(TokenType.Literal, "\\"));
-                }
-            },
-            '+' => {
-                if (peek(input, i)) |s| {
-                    switch (s) {
-                        '+' => {
-                            try outputTokens.append(createToken(TokenType.PlusPlus, ""));
-                            i += 1;
-                        },
-                        '=' => {
-                            try outputTokens.append(createToken(TokenType.PlusEqual, ""));
-                            i += 1;
-                        },
-                        else => {
-                            try outputTokens.append(createToken(TokenType.Plus, ""));
-                        }
-                    }
-                }
-                else { // lowkey probably just err, why would it end with an operator?
-                    try outputTokens.append(createToken(TokenType.Plus, ""));
-                }
-                // try outputTokens.append(createToken(TokenType.Comma, ""));
-            },
-            '-' => {
-                if (peek(input, i)) |s| {
-                    switch (s) {
-                        '-' => {
-                            try outputTokens.append(createToken(TokenType.MinusMinus, ""));
-                            i += 1;
-                        },
-                        '=' => {
-                            try outputTokens.append(createToken(TokenType.MinusEqual, ""));
-                            i += 1;
-                        },
-                        else => {
-                            try outputTokens.append(createToken(TokenType.Minus, ""));
-                        }
-                    }
-                }
-                else { // lowkey probably just err, why would it end with an operator?
-                    try outputTokens.append(createToken(TokenType.Minus, ""));
-                }
-                // try outputTokens.append(createToken(TokenType.Comma, ""));
-            },
-            '*' => {
-                if (peek(input, i)) |s| {
-                    if (s == '=') {
-                        try outputTokens.append(createToken(TokenType.StarEqual, ""));
-                        i += 1;
-                    }
-                    else {
-                        try outputTokens.append(createToken(TokenType.Star, ""));
-                    }
-                } else {
-                    try outputTokens.append(createToken(TokenType.Star, ""));
-                }
-            },
-            '/' => {
-                if (peek(input, i)) |s| {
-                    if (s == '=') {
-                        try outputTokens.append(createToken(TokenType.SlashEqual, ""));
-                        i += 1;
-                    }
-                    else {
-                        try outputTokens.append(createToken(TokenType.Slash, ""));
-                    }
-                } else {
-                    try outputTokens.append(createToken(TokenType.Slash, ""));
-                }
-            },
-            '%' => {
-                if (peek(input, i)) |s| {
-                    if (s == '=') {
-                        try outputTokens.append(createToken(TokenType.ModEqual, ""));
-                        i += 1;
-                    }
-                    else {
-                        try outputTokens.append(createToken(TokenType.Mod, ""));
-                    }
-                } else {
-                    try outputTokens.append(createToken(TokenType.Mod, ""));
-                }
-            },
-            else => {
-            },
+    fn peek(self: *Tokenizer) ?u8 {
+        if (self.i + 1 >= self.input.len) {
+            return null;
         }
 
-        // std.debug.print("{any}", .{i});
-        i += 1;
+        return self.input[self.i + 1];
     }
-    
-    printTokens(outputTokens);
-    return outputTokens;
+
+    fn printTokens(input: std.ArrayList(Token)) void {
+        for (input.items) |token| {
+            switch (token) {
+                .Literal => |str| std.debug.print("Literal: {s}\n", .{str}),
+                .EscapedChar => |c| std.debug.print("EscapedChar: {s}\n", .{&[1]u8 {c}}),
+                else => std.debug.print("{s}\n", .{@tagName(token)})
+            }
+        }
+    }
+
+    fn xOrxEqual(self: *Tokenizer, T1: Token, T2: Token) Token {
+        var T = T1;
+        if (self.peek()) |c| {
+            if (c == '=') {
+                self.i += 1;
+                T = T2;
+            }
+        }
+
+        return T;
+    }
+
+    pub fn tokenize(self: *Tokenizer) !std.ArrayList(Token) {
+        while (self.i < self.input.len) {
+            var T: ?Token = null;
+            switch (self.input[self.i]) {
+                '(' => T = Token.LParen,
+                ')' => T = Token.RParen,
+                '[' => T = Token.LBracket,
+                ']' => T = Token.RBracket,
+                '{' => T = Token.LCurly,
+                '}' => T = Token.RCurly,
+                '.' => T = Token.Dot,
+                ',' => T = Token.Comma,
+                '$' => T = Token.Dollar,
+                ';' => T = Token.Semicolon,
+                '\"' => T = Token.DoubleQuote,
+                '\'' => T = Token.SingleQuote,
+                '\\' => {
+                    if (self.peek()) |c| {
+                        T = Token{.EscapedChar = c};
+                        self.i += 1;
+                    } else {
+                        // try out.append(Token{.Literal = "\\"});
+                        T = Token{.Literal = "\\"};
+                    }
+                },
+                '+' => T = self.xOrxEqual(Token.Plus, Token.PlusEqual),
+                '-' => T = self.xOrxEqual(Token.Minus, Token.MinusEqual),
+                '*' => T = self.xOrxEqual(Token.Star, Token.StarEqual),
+                '/' => T = self.xOrxEqual(Token.Slash, Token.SlashEqual),
+                '%' => T = self.xOrxEqual(Token.Mod, Token.ModEqual),
+                '&' => T = self.xOrxEqual(Token.Ampersand, Token.ANDEqual),
+                '|' => T = self.xOrxEqual(Token.Pipe, Token.OREqual),
+                '^' => T = self.xOrxEqual(Token.XOR, Token.XOREqual),
+                '!' => T = self.xOrxEqual(Token.NOT, Token.BangNotEqual),
+                '=' => T = self.xOrxEqual(Token.Equal, Token.IF_Equal),
+                else => {}
+            }
+
+            if (T) |token| {
+                try self.out.append(token);
+            }
+
+            self.i += 1;
+        }
+
+        printTokens(self.out);
+        return self.out;
+    }
+};
+
+pub fn tokenize(input: []const u8) !std.ArrayList(Token) {
+    var tokenizer = Tokenizer{
+        .i = 0,
+        .input = input,
+        .out = std.ArrayList(Token).init(allocator)
+    };
+
+    tokenizer.init();
+    return tokenizer.tokenize();
+    // const T: ?Token = null;
+    // return T;
+    // return tokenizer.out;
 }
